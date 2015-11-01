956ce0a0c815ea20785fac7a44659cc2c52ca156
Jessica <jessicajoly88@gmail.com>
2015-11-01 12:36:03 -0800

    Refactor hidden menu navigation bar.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,38 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree 
-//= require nav_menu.js
+
+$(document).ready(function(){                       
+
+       //Navigation Menu Slider
+        $('#nav-expander').on('click',function(e){
+          e.preventDefault();
+          $('body').toggleClass('nav-expanded');
+        });
+        $('#nav-close').on('click',function(e){
+          e.preventDefault();
+          $('body').removeClass('nav-expanded');
+        });
+
+        // Initialize navgoco with default options
+        $(".main-menu").navgoco({
+            caret: '<span class="caret"></span>',
+            accordion: false,
+            openClass: 'open',
+            save: true,
+            cookie: {
+                name: 'navgoco',
+                expires: false,
+                path: '/'
+            },
+            slide: {
+                duration: 300,
+                easing: 'swing'
+            }
+        });
+
+      });
+
+
 
 
