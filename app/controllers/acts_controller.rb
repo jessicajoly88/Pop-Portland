dbb700ae19632163ab456774414d5c1223b5c2e0
Jessica <jessicajoly88@gmail.com>
2015-11-01 13:57:26 -0800

    Add the add an act functionality to index page.

---
+++ app/controllers/acts_controller.rb
@@ -0,0 +1,26 @@
+class ActsController < ApplicationController
+
+
+  def new 
+  	@act = Act.new
+  end 
+
+  def create 
+  	@act = Act.new(act_params)
+  	  if @act.save 
+  	  	respond_to do |format|
+  	  	  format.html { redirect_to root }
+  	  	  format.js
+  	  	end
+  	  else
+  	  	render :new
+  	  end
+  end 
+
+
+private
+  def act_params
+    params.require(:act).permit(:name, :genre, :description)
+  end
+end
+
