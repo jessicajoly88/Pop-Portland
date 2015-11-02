14e37cd930428a0526578b820b4c7aa289152758
Jessica <jessicajoly88@gmail.com>
2015-11-02 13:52:22 -0800

    Indent JavaScript in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,39 +1,35 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree 
+//= require_tree
 
-$(document).ready(function(){                       
+$(document).ready(function(){
 
-       //Navigation Menu Slider
-        $('#nav-expander').on('click',function(e){
-          e.preventDefault();
-          $('body').toggleClass('nav-expanded');
-        });
-        $('#nav-close').on('click',function(e){
-          e.preventDefault();
-          $('body').removeClass('nav-expanded');
-        });
+    //Navigation Menu Slider
+    $('#nav-expander').on('click',function(e){
+      e.preventDefault();
+      $('body').toggleClass('nav-expanded');
+    });
+    $('#nav-close').on('click',function(e){
+      e.preventDefault();
+      $('body').removeClass('nav-expanded');
+    });
 
-        // Initialize navgoco with default options
-        $(".main-menu").navgoco({
-            caret: '<span class="caret"></span>',
-            accordion: false,
-            openClass: 'open',
-            save: true,
-            cookie: {
-                name: 'navgoco',
-                expires: false,
-                path: '/'
-            },
-            slide: {
-                duration: 300,
-                easing: 'swing'
-            }
-        });
+    // Initialize navgoco with default options
+    $(".main-menu").navgoco({
+        caret: '<span class="caret"></span>',
+        accordion: false,
+        openClass: 'open',
+        save: true,
+        cookie: {
+            name: 'navgoco',
+            expires: false,
+            path: '/'
+        },
+        slide: {
+            duration: 300,
+            easing: 'swing'
+        }
+    });
 
-      });
-
-
-
-
+});
