8e9ff166717c77f49b9ab18137f2ccab0c089b73
Jessica <jessicajoly88@gmail.com>
2015-11-03 13:06:38 -0800

    Add song url to 6 individual acts.

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -2,5 +2,11 @@ require 'rails_helper'
 
 describe Song do
   it {should belong_to :act}
-  it {should validate_presence_of :html}
+  it {should validate_presence_of :url}
+
+
+  # it "doesn't save the song if SoundCloud gives an error" do
+  #   song = Song.new(:url => 'www.https://soundcloud.com/record/junius-meyvant-color-')
+  #   song.save.should be false
+  # end
 end
