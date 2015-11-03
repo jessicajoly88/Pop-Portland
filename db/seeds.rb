8b3af03e31f4cb8b205f958ce78b564250d19290
Jessica <jessicajoly88@gmail.com>
2015-11-03 15:39:01 -0800

    Seed events in DB, on individual act page, show all events that it belongs to. Styling very poor at moment.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,9 @@ act_list = [
 
   ["FM Belfast(IS)","Icelandic Folk-Electro", "The President of Iceland, Mr. Ólafur Ragnar Grímsson, often speaks of the special nature of the Icelandic people. In his mind, the most treasured commodity among Icelanders is their “Viking” spirit—their passion and enthusiasm for conquering new ground and reaping the benefits of their adventurous and courageous natures. This is obviously a load of nonsense and has absolutely nothing to do with FM Belfast. FM Belfast just want to be your friend. Because FM Belfast are not Vikings. They do not pillage and plunder. Instead, they give, and they care. With simple, delicately crafted melodies, unpredictable beats and humorous lyrics, FM Belfast bring smiles to their audience’s faces, and life to their every extremity—whether it be in the car, in the kitchen or on the dancefloor. But there’s more. In their live performances, FM Belfast somehow manage to elevate their music to new heights. They manage to mediate a certain weltanschauung; manage to share their joie de vivre. Seeing an FM Belfast concert is almost a religous experience, save for the religion. Unlike organized religion, though, their music isn’t conditional—it is simply inviting. It offers a genuine sense of belonging. Everyone feels it, as they dance and chant and release more sweat than Jane Fonda’s disciples ever could. FM Belfast was originally formed by couple Lóa Hjálmtýsdóttir and Árni Hlöðversson in late 2005. They wanted to record a song, one which they could give to their friends as a Christmas present. Says a lot, doesn’t it? Not surprisingly, the Iceland Airwaves music festival proved to be a catalyst in the band’s story—as Lóa and Árni needed extra people for their live act in 2006. These extra people have been numerous, although the core members have included Árni Vilhjálmsson (Motherfuckers in the House), Örvar Þóreyjarson Smárason (Múm). Andri Snær Magnason, three-time recipient of the Icelandic Literary Prize, says he has to attend a FM Belfast concert at least once every six months, according to doctor's orders. It is worth mentioning that Andri Snær is not a doctor, but his father is a doctor; his grandfather is a doctor; his sister is no less than a neurosurgeon! Should we, the common, soot-stained masses, doubt the professional medical advice of an entire family? Thankfully we don't have to. Because FM Belfast takes its role in the public's health very seriously. Because the members of the band are just like you and me—they are a part of us. They pay off their mortgages, take their kids to day care and in the evening boil potatoes. They don't always have enough to fill the gas tank or perhaps don't even have a car to begin with. They've even been known to play basketball in tattered sneakers. The band's struggle for survival, and that of the Icelandic nation as a whole, is wearisome and tedious. But in the end, there is good news, because FM Belfast knows the antidote—the recipe for making the struggle a happy one and for patching the holes in tattered sneakers.","https://www.fmbelfast.com/", "http://icelandairwaves.is/artist-data/fullsize/389298.jpg", "https://soundcloud.com/fmbelfast/brighter-days" ],
 
-  ["Júníus Meyvant","Icelandic Folk","Júníus Meyvant is the artist name of Vestmann Islands born singer-songwriter Unnar Gísli Sigurmundsson. As a young boy he was an eccentric to a small degree and he only loved two things, skateboarding and painting. Every now and then he asked the Muses if he should learn to play an instrument but his wild and free behavior terminated his dreams as he was quickly suspended from music school. All through his teens he rambled on without even considering becoming a musician or even playing any instruments. In his early twenties Unnar stumbled upon a beat up guitar at his parents’ house and started noodling around. By playing the guitar he managed to tame his inner beast and his sense for songs and melodies burst like rockets. The uncontrollable urge for writing music and constant flow of ideas kept him tossing and turning every night and day forever and a day. For a while he filtered his ideas through a band that didn’t live up to his creative requirements so he decided to undertake his alter ego Júníus Meyvant. The sound of Júníus Meyvant is a rich and afflicted take on freaky folk pop with a familiar and soulful feel. The feeling you get from listening to Júníus is equally as comfortable as resting under a thick wool blanket with a hot cup of cocoa or even when one settles with an exotic beverage on a hot sandy beach far and away.", "http://www.juniusmeyvant.com/","http://icelandairwaves.is/artist-data/fullsize/362509.jpg","https://soundcloud.com/recordrecords/junius-meyvant-color-decay"]
+  ["Júníus Meyvant","Icelandic Folk","Júníus Meyvant is the artist name of Vestmann Islands born singer-songwriter Unnar Gísli Sigurmundsson. As a young boy he was an eccentric to a small degree and he only loved two things, skateboarding and painting. Every now and then he asked the Muses if he should learn to play an instrument but his wild and free behavior terminated his dreams as he was quickly suspended from music school. All through his teens he rambled on without even considering becoming a musician or even playing any instruments. In his early twenties Unnar stumbled upon a beat up guitar at his parents’ house and started noodling around. By playing the guitar he managed to tame his inner beast and his sense for songs and melodies burst like rockets. The uncontrollable urge for writing music and constant flow of ideas kept him tossing and turning every night and day forever and a day. For a while he filtered his ideas through a band that didn’t live up to his creative requirements so he decided to undertake his alter ego Júníus Meyvant. The sound of Júníus Meyvant is a rich and afflicted take on freaky folk pop with a familiar and soulful feel. The feeling you get from listening to Júníus is equally as comfortable as resting under a thick wool blanket with a hot cup of cocoa or even when one settles with an exotic beverage on a hot sandy beach far and away.", "http://www.juniusmeyvant.com/","http://icelandairwaves.is/artist-data/fullsize/362509.jpg","https://soundcloud.com/recordrecords/junius-meyvant-color-decay"],
 
   ["Kiasmos", "Electronic", "Kiasmos is made up of Icelandic BAFTA-winning composer Ólafur Arnalds, known for his unique blend of minimal piano and string compositions with electronic sounds, and Janus Rasmussen from the Faroe Islands, known as the mastermind of the electro-pop outfit Bloodgroup. Based in Reykjavík, Arnalds used to work as a sound engineer, often for Rasmussen's other projects, where the two musicians discovered their common love for minimal, experimental music. They eventually became best friends, often hanging out in their studio, exploring electronic sounds. After dropping several tracks and performing at select festivals throughout the years, Ólafur Arnalds and Janus Rasmussen are now dedicating their time to exploring the area in-between Ólafur’s more acoustic, piano-based solo work and Janus’s synth-heavy electro pop, with their collaborative electronic project Kiasmos.","http://www.erasedtapes.com/artists/biography/11/kiasmos", "http://icelandairwaves.is/artist-data/fullsize/395238.jpg", "https://soundcloud.com/erasedtapes/kiasmos-bent" ]
-
 
 ]
 
@@ -25,25 +24,26 @@ end
 
 venue_list = [
 
-0  ["McMenamins Crystal Ballroom", "1332 W Burnside St, Portland, OR 97209", "Spring-loaded dance floor adds bounce to concert venue & club with tall windows & house-brewed beer."],
+  ["McMenamins Crystal Ballroom", "1332 W Burnside St, Portland, OR 97209", "Spring-loaded dance floor adds bounce to concert venue & club with tall windows & house-brewed beer."],
 
-1  ["Mississippi Studios and Bar Bar","3939 N Mississippi Ave, Portland, OR 97227","A renovated church houses this intimate live music spot with burgers & drinks in the adjoining bar."],
+  ["Mississippi Studios and Bar Bar","3939 N Mississippi Ave, Portland, OR 97227","A renovated church houses this intimate live music spot with burgers & drinks in the adjoining bar."],
 
-2  ["Valentine's","232 SW Ankeny St, Portland, OR 97204","Petite 2-level bar with a bohemian vibe & outdoor seating offering live music, DJs & art shows."],
+   ["Valentine's","232 SW Ankeny St, Portland, OR 97204","Petite 2-level bar with a bohemian vibe & outdoor seating offering live music, DJs & art shows."],
 
-3  ["Roseland Theater","8 NW 6th Ave, Portland, OR 97209", "Cozy, mostly standing-room-only concert hall with a storied history & cash-only tickets & drinks."],
+   ["Roseland Theater","8 NW 6th Ave, Portland, OR 97209", "Cozy, mostly standing-room-only concert hall with a storied history & cash-only tickets & drinks."],
 
-4  ["Doug Fir Restaurant and Lounge", "830 E Burnside St, Portland, OR 97214","Spot with futuristic log-cabin decor hosting indie music shows, with an eclectic diner menu & bar"],
+   ["Doug Fir Restaurant and Lounge", "830 E Burnside St, Portland, OR 97214","Spot with futuristic log-cabin decor hosting indie music shows, with an eclectic diner menu & bar"],
 
-5  ["Aladdin Theater","3017 SE Milwaukie Ave, Portland, OR 97202", "1920s former movie house now presents live music & comedy shows in a cozy auditorium."],
+   ["Aladdin Theater","3017 SE Milwaukie Ave, Portland, OR 97202", "1920s former movie house now presents live music & comedy shows in a cozy auditorium."],
 
-6  ["Wonder Ballroom","128 NE Russell St, Portland, OR 97212", "Live-music venue in a historic former church featuring some balcony seats & a lounge with nibbles."],
+   ["Wonder Ballroom","128 NE Russell St, Portland, OR 97212", "Live-music venue in a historic former church featuring some balcony seats & a lounge with nibbles."],
 
-7  ["Alberta Rose Theatre","3000 NE Alberta St, Portland, OR 97211","Renovated movie house from the 1920s presents live music, comedy & vaudeville in an intimate space."],
+   ["Alberta Rose Theatre","3000 NE Alberta St, Portland, OR 97211","Renovated movie house from the 1920s presents live music, comedy & vaudeville in an intimate space."],
 
-8  ["Goodfoot Pub & Lounge","2845 SE Stark St, Portland, OR 97214","Upstairs pub offers pool, pinball, bar food & beers on tap, while basement hosts live acts & DJs."],
+   ["Goodfoot Pub & Lounge","2845 SE Stark St, Portland, OR 97214","Upstairs pub offers pool, pinball, bar food & beers on tap, while basement hosts live acts & DJs."],
 
-9  ["Arlene Schnitzer Concert Hall","1037 SW Broadway, Portland, OR 97205", "Romantic, high-ceilinged performing-arts venue features diverse shows in an Italian rococo building."]
+   ["Arlene Schnitzer Concert Hall","1037 SW Broadway, Portland, OR 97205", "Romantic, high-ceilinged performing-arts venue features diverse shows in an Italian rococo building."]
+
 ]
 
 venue_list.each do |name, address, description|
@@ -70,6 +70,6 @@ event_list = [
 
 ]
 
-event_list.each do |time_period, date, act_id, venue_id|
-  Event.create(time_period: time_period, date: date, venue_id: venue_id, act_id: act_id)
+event_list.each do |act_id, venue_id, time_period, date|
+  Event.create(act_id: act_id, venue_id: venue_id, date: date,time_period: time_period)
 end
